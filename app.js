eb3e83201b730d1f1fd702fa87d942bb912e19ce
Woomin Jo <111989178+umin5056@users.noreply.github.com>
2022-10-15 11:14:38 +0900

    22.10.13 mmt

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const LoginForm = document.querySelector(".login-form");
-const LoginInput = document.querySelector(".login-form input");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASS = "hidden";
 
-function LoginSubmit(Event) {
-  Event.preventDefault();
-  console.log(Event);
+function loginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASS);
+  const username = loginInput.value;
+  greeting.classList.remove(HIDDEN_CLASS);
+  greeting.innerText = `Hello~ ${username}`;
 }
 
-LoginForm.addEventListener("submit", LoginSubmit);
+loginForm.addEventListener("submit", loginSubmit);
